5ec9eecfa732673edf5f5dd8ec85888aca58b6ec
Ahneb <benjaminha1996@gmail.com>
2023-02-25 05:31:07 -0800

    added login api

--- controller/apis/user.js
+++ controller/apis/user.js
@@ -2,6 +2,9 @@ const { Router } = require('express');
 
 const { User } = require('../../models');
 
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
 const userRouter = new Router();
 
 userRouter.post('/', async (req, res) => {
@@ -15,6 +18,7 @@ userRouter.post('/', async (req, res) => {
 
   if (existingUser) {
     res.status(409).end();
+    return;
   };
 
   const newUser = await User.create({
@@ -28,3 +32,33 @@ userRouter.post('/', async (req, res) => {
 });
 
 module.exports = userRouter;
+
+userRouter.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+
+  const existingUser = await User.findOne({
+    where: {
+      email,
+    },
+  });
+
+  if (!existingUser) {
+    res.status(401).end();
+    return;
+  };
+
+  if (!bcrypt.compareSync(password, existingUser.password)) {
+    res.status(401).end();
+    return;
+  };
+
+  const token = jwt.sign({
+    id: existingUser.id,
+  }, process.env.JWT_TOKEN);
+
+  res.cookie('session_token', token, {
+    maxAge: 1000 * 60 * 60* 5,
+  });
+
+  res.status(200).end(); 
+});
