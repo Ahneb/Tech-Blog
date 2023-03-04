b41d4b4307ef5b082250b2ccd02679d74239e0c0
Ahneb <benjaminha1996@gmail.com>
2023-03-04 12:29:46 -0800

    changed the require statement from models to /models/user

--- controller/apis/user.js
+++ controller/apis/user.js
@@ -1,5 +1,5 @@
 const { Router } = require('express');
-const { User } = require('../../models');
+const { User } = require('../../models/User');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
