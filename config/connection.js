30f00bdaf38062d54436220eef86e781b3cf7243
Ahneb <benjaminha1996@gmail.com>
2023-03-04 13:12:28 -0800

    added cleardburl for sequelize initiation on heroku

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,19 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-let sequelize = new Sequelize(
-  process.env.DB_DB,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-  }
-)
+let sequelize;
+if (process.env.CLEARDB_DATABASE_URL) {
+  sequelize = new Sequelize(process.env.CLEARDB_DATABASE_URL);
+} else {
+  new Sequelize(
+    process.env.DB_DB,
+    process.env.DB_USER,
+    process.env.DB_PASS,
+    {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+    }
+  )
+};
 
 module.exports = sequelize;
